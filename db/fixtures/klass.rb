38211efb75b81fc6a1682113f7df9031fe442cd7
Cody Maggard <cody@heliumsyndicate.com>
2008-04-02 16:19:57 -0500

    Fixing string array

--- db/fixtures/klass.rb
+++ db/fixtures/klass.rb
@@ -1,4 +1,4 @@
-%{Druid Hunter Mage Paladin Priest Rogue Shaman Warlock Warrior}.each do |k|
+%w{Druid Hunter Mage Paladin Priest Rogue Shaman Warlock Warrior}.each do |k|
   Klass.find_or_create_by_name(k)
 end
 
