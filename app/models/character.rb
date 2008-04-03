9fdd0823f42623edb75e04a374c86e1df36ac8bd
Cody Maggard <cody@heliumsyndicate.com>
2008-04-03 01:06:35 -0500

    added min_level and max_level named scopes

--- app/models/character.rb
+++ app/models/character.rb
@@ -15,5 +15,6 @@ class Character < ActiveRecord::Base
   end
   
   named_scope :level, lambda { |l| { :conditions => ["level = ?", l] } }
-  
+  named_scope :min_level, lambda { |l| { :conditions => ["level > ?", l] } }
+  named_scope :max_level, lambda { |l| { :conditions => ["level < ?", l] } }  
 end
