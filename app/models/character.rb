f9a96a4d73df40237dc6bd7ec635a23954a97651
Cody Maggard <cody@heliumsyndicate.com>
2008-04-03 01:03:13 -0500

    Faction named_finders now, also level(x) named finder that finds that level only.  Needs level_min and level_max.

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,11 +6,14 @@ class Character < ActiveRecord::Base
   belongs_to :klass
   belongs_to :faction
   
-  [Gender, Race, Klass].each do |c|
+  [Gender, Race, Klass, Faction].each do |c|
     c.find(:all).each do |g|
       named_scope g.name.downcase.gsub(" ","_").to_sym,
         { :conditions => {c.to_s.downcase.pluralize + ".name" => g.name}, 
           :include => c.to_s.downcase.to_sym }
     end
   end
+  
+  named_scope :level, lambda { |l| { :conditions => ["level = ?", l] } }
+  
 end
