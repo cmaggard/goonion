0537e4bebbf369bf00a78db34a7cbac8286eac56
Cody Maggard <cody@heliumsyndicate.com>
2008-04-04 13:11:46 -0500

    Generating Skills megatable

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080402231206) do
+ActiveRecord::Schema.define(:version => 20080404181120) do
 
   create_table "characters", :force => true do |t|
     t.string   "name"
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(:version => 20080402231206) do
 
   create_table "servers", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "skills", :force => true do |t|
+    t.string   "name"
+    t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
