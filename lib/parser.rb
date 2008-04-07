6a511486c41319d2f30e630608367ba73bac0da5
Cody Maggard <cody@heliumsyndicate.com>
2008-04-06 23:24:17 -0500

    Massive refactor.

--- lib/parser.rb
+++ lib/parser.rb
@@ -14,40 +14,38 @@ class Parser
                     "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-GB; rv:1.8.1.4) Gecko/20070515 Firefox/2.0.0.4",
                    "connection" => "close"}
   
-  attr_accessor :realm_url, :guild_url, :realm, :guild, :realm_id, :guild_id, :guild_realm_set
-  def initialize(realm = "Mal'Ganis", guild = "Goon Squad")
-    self.realm = realm
-    self.guild = guild
-    self.guild_realm_set = false
-  end
-
-  def parse!
+  def self.parse_guild(server = "Mal'Ganis", guild = "Goon Squad")!
     begin
-      characters = retrieve_guild_members
+      characters = retrieve_guild_members(server, guild)
       Rails.logger.debug "Guild members retrieved.  Count: #{characters.length}"
-      
-      retrieve_realm_and_guild_ids
-      
+    
       characters.each do |c|
-        parse_character(c)
+        parse_character(server, c, guild)
+        yield "."
+      end
+        
+      # Can't get guild faction from guild page; if Guild does not have faction set, 
+      g = Server.find_by_name(server).guilds.find_by_name(guild)
+      if g.faction = nil
+        faction = g.characters.find(:first).faction
+        g.update_attribute(:faction, faction)
       end
     rescue Exception => e
       raise e
     end
   end
-    
-  def parse_character(c)
+  
+  def self.parse_character(server_name, character_name, guild_name = nil)
     begin
-      char = Server.find_by_name(realm_id.name).characters.find_or_create_by_name(c)
-      
+      char = Server.find_or_create_by_name(server_name).characters.find_or_create_by_name(character_name)
+    
       unless char.created_at == char.updated_at
         return if char.updated_at > 1.day.ago
       end
-      
-      xml = Hpricot.XML(open(URI.escape(CHARACTER_SHEET_URL % [realm, c]), REQUEST_HASH))
+    
+      xml = Hpricot.XML(open(URI.escape(CHARACTER_SHEET_URL % [server_name, character_name]), REQUEST_HASH))
 
-      char.guild = self.guild_id
-      char.server = self.realm_id
+      char.server, char.guild = retrieve_server_and_guild_ids(server_name, guild_name)
 
       characterInfo = (xml % :page % :characterInfo)      
       character = (characterInfo % :character)
@@ -67,16 +65,10 @@ class Parser
       # end
       ############################################################
 
-      
-      # Can't get guild faction from guild page; if Guild does not have faction set, 
-      unless guild_realm_set
-        char.guild.update_attribute(:faction, char.faction) 
-        guild_realm_set = false
-      end
       char.save
       # Increase to 2.0 or 3.0 if seeing more errors
-      sleep 1.0
-      
+      sleep 2.0
+    
     rescue OpenURI::HTTPError => e
       sleep 5.0
       retry
@@ -86,11 +78,11 @@ class Parser
     end
   end
 
-  def retrieve_guild_members
-    roster = Hpricot.XML(open(URI.escape(GUILD_ROSTER_URL % [realm, guild]), REQUEST_HASH))
+  def self.retrieve_guild_members(server, guild)
+    roster = Hpricot.XML(open(URI.escape(GUILD_ROSTER_URL % [server, guild]), REQUEST_HASH))
     begin
       memberlist = (roster % :page % :guildInfo % :guild % :members)
-    
+  
       (memberlist / :character).inject([]) do |k,v|
         k << v[:n]
       end
@@ -98,12 +90,12 @@ class Parser
       raise NoSuchGuildError
     end  
   end
-  
+
   protected 
-  
-  def retrieve_realm_and_guild_ids
-    self.realm_id = Server.find_or_create_by_name(realm)
-    self.guild_id = self.realm_id.guilds.find_or_create_by_name(guild)
+
+  def self.retrieve_server_and_guild_ids(server, guild)
+    server_id = Server.find_or_create_by_name(server)
+    guild_id = server_id.guilds.find_or_create_by_name(guild)
+    return server_id, guild_id
   end
-  
 end
