a644cfb67bc47c4df675dd48d72cf9d58102d558
Cody Maggard <cody@heliumsyndicate.com>
2008-04-07 13:28:02 -0500

    Adding Secondary Tradeskills class and fixture

---
+++ app/models/secondary.rb
@@ -0,0 +1,4 @@
+class Secondary < Skill
+  has_many :skill_levels, :foreign_key => "skill_id"
+  has_many :characters, :through => :skill_levels
+end
---
+++ db/fixtures/secondary.rb
@@ -0,0 +1,5 @@
+Secondary.find_or_create_by_name("First Aid")
+Secondary.find_or_create_by_name("Fishing")
+Secondary.find_or_create_by_name("Cooking")
+
+puts "Secondary skills seeded."
