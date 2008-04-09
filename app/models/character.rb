a51e81a5344dc48df759d5bd9e97125f9b140adb
Cody Maggard <cody@heliumsyndicate.com>
2008-04-09 16:18:18 -0500

    add_skills implemented

--- app/models/character.rb
+++ app/models/character.rb
@@ -7,6 +7,7 @@ class Character < ActiveRecord::Base
   belongs_to :faction
 
   has_many :skill_levels
+  has_many :skills, :through => :skill_levels
   has_many :professions, :class_name => "Profession", :source => :skill, :through => :skill_levels
   has_many :secondaries, :class_name => "Secondary", :source => :skill, :through => :skill_levels
   
@@ -23,16 +24,36 @@ class Character < ActiveRecord::Base
       lambda { |n| { :conditions => {c.to_s.downcase.pluralize + ".name" => n},
                      :include => c.to_s.downcase.to_sym } }
   end
-  
-  ["level"].each do |c|
-    { "" => "=", "min_" => ">=", "max_" => "<="}.each do |k, v|
-      named_scope( (k+c).to_sym,
-        lambda { |l| { :conditions => [[c,v].join(" ") + " ?", l] } })
-    end
+
+  { "" => "=", "min_" => ">=", "max_" => "<="}.each do |k, v|
+    named_scope( (k+"level").to_sym,
+      lambda { |l| { :conditions => [["level",v].join(" ") + " ?", l] } })
   end
   
-  def add_skill(type,name,level)
-    klass = eval(type.gsub("skills","").singularize.capitalize)
-    skill = klass.send(:find_by_name, name)
+  # skill_hash is :skill_name => level
+  def add_skills(skill_hash)
+    char_skill_levels = self.skill_levels
+    
+    update_hash = Hash.new
+    
+    char_skill_levels.each do |sk|
+      update_hash[sk.skill.name] = sk
+    end
+
+    skill_hash.each do |name, value|
+      begin
+        update_hash[name].level = value
+      rescue NoMethodError
+        sk = Skill.find_by_name( name )
+        sl = SkillLevel.new
+        sl.level = level
+        sl.skill = sk
+        skill_levels << sl
+      end
+    end
+    
+    update_hash.each do |k,v|
+      v.save if v.changed?
+    end
   end
 end
