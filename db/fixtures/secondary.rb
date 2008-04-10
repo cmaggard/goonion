877cdf6fd2c3023887f760a11f69c496f8384cfa
Cody Maggard <cody@heliumsyndicate.com>
2008-04-10 15:24:16 -0500

    Adding Riding as a secondary skill

--- db/fixtures/secondary.rb
+++ db/fixtures/secondary.rb
@@ -1,5 +1,6 @@
 Secondary.find_or_create_by_name("First Aid")
 Secondary.find_or_create_by_name("Fishing")
 Secondary.find_or_create_by_name("Cooking")
+Secondary.find_or_create_by_name("Riding")
 
 puts "Secondary skills seeded."
